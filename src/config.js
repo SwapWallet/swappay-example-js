7de62b816614f3ce23ee88a371fe7c84d7ed78fe
Sadegh Hayeri <sadeghhayeri@sotoon.ir>
2023-08-30 13:16:59 +0330

    Make mongo private

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const config = {
         apiKey: '',
     },
     db: {
-        uri: 'mongodb://root:rootpassword@127.0.0.1:27017/?directConnection=true'
+        uri: 'mongodb://root:rootpassword@mongodb:27017/?directConnection=true'
     },
 }
 
